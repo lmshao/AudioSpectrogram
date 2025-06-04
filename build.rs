277277d765833e36e094d794dd9197ee8ba6ba0c
SHAO Liming <lmshao@163.com>
2025-06-04 23:26:51 +0800

    add Windows version info

--- build.rs
+++ build.rs
@@ -10,4 +10,8 @@ fn main() {
 
     let build_time = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     write!(f, "pub const BUILD_TIME: &str = \"{}\";", build_time).unwrap();
+
+    if cfg!(target_os = "windows") {
+        let _ = embed_resource::compile("resources.rc", Vec::<&str>::new());
+    }
 }
